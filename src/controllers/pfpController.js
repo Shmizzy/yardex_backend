c943411bc74acbf8540770c14e50d424047098fc
izzylppz <https://github.com/Shmizzy>
2024-08-06 19:20:59 -0500

    servicerpfp

--- src/controllers/pfpController.js
+++ src/controllers/pfpController.js
@@ -21,7 +21,7 @@ router.post('/upload/servicer', (middleware.isServicer), async (req, res) => {
         if (!req.user)
             return res.status(401).json({ message: 'Authentication required' });
 
-        const profile = await Servicer.findByIdAndUpdate(req.user.id, { pfp: req.body.image }, { new: true });
+        const profile = await Servicer.findOneAndUpdate({user: req.user.id}, { pfp: req.body.image }, { new: true });
         res.status(200).json({ profile });
     } catch (error) {
         res.status(400).json({ message: error.message });
@@ -43,7 +43,7 @@ router.get('/profile/servicer', (middleware.isServicer), async (req, res) => {
     try {
         if (!req.user)
             return res.status(401).json({ message: 'Authentication required' });
-        const profile = await Servicer.findById(req.user.id);
+        const profile = await Servicer.findOne({user: req.user.id});
         res.status(200).json({ profile });
     } catch (error) {
         res.status(400).json({ message: error.message });
